3311c3ebd407a146d6036b7106b05a57b7a0aa98
zz3704 <zz3704@naver.com>
2023-09-25 12:21:39 +0900

    week05 v1.0

--- week05_1.go
+++ week05_1.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	dice := rand.Intn(6) + 1 //0~5 add 1 to make dice number(1~6)
-	fmt.Println(dice)
+	rand.Seed(time.Now().Unix())
+	answer := rand.Intn(100) + 1
+	fmt.Println("guess game(1~100) : ")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i > 10; i++ {
+		fmt.Println("you chance : ", 10-i)
+		fmt.Print("guess number : ")
+		inputNumber, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputNumberString = string.TrimString(inputNumberstring)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputNumber < answer {
+			fmt.Println("ur guess number is lower than answer.") //answer is higher
+		} else if inputNumber > answer {
+			fmt.Println("ur guess number is higher than answer.") //answer is lower
+		}
+	}
 }

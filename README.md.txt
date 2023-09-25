d0eff4d5ad8c1d34e59f6927885fe569b60dad27
zz3704 <zz3704@naver.com>
2023-09-25 10:39:28 +0900

    week05 v0.2

---
+++ README.md.txt
@@ -0,0 +1,1 @@
+This repository stores/manages the go language source code used in the 2023 open source programming class.
--- week05_1.go
+++ week05_1.go
@@ -1,9 +1,14 @@
 package main
 
-import()
+import (
+	"time"
+	"fmt"
+)
 
 func main()  {
 	var now time.Time=time.now()
 	var year int = now.year()
+	month := now.Month()
+	fmt.println(now.Month)
 	fmt.println(year)
 }

ba2a7241739ac3e4c8fcbcd5ad62a5f834fd76d2
zz3704 <zz3704@naver.com>
2023-09-25 11:34:43 +0900

    week05 v0.6

--- week05_1.go
+++ week05_1.go
@@ -10,7 +10,10 @@ import (
 func main() {
 	fmt.Print("정수 입력 : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n')
+	inputScore, err := reader.ReadString('\n') //option2
+	if err !=nil{  //conditionals
+		log.Fatal(err)
+	}
 	log.Fatal(err)
 	fmt.Println(inputScore)
 }
